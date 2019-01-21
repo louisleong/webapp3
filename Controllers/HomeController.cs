41d45663aceac86fb211318d4556de55e1e88a9a
Louis Leong <louis@live.com.sg>
2019-01-21 11:48:21 +0800

    awww

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace webapp.Controllers
 
         public IActionResult Privacy()
         {
-            return View();
+            return View()
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
